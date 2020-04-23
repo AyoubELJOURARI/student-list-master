35d7a1c7a0698ceb8a73124765bc80e28cab147f
AyoubJr <57492334+AyoubJr@users.noreply.github.com>
2020-04-23 21:46:55 +0000

    update
    ok

--- simple_api/student_age.py
+++ simple_api/student_age.py
@@ -1,5 +1,4 @@
 #!flask/bin/python
-#!flask/bin/python
 from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
@@ -28,19 +27,19 @@ def unauthorized():
 
 try:
     student_age_file_path
-    student_age_file_path  = os.environ['student_age_file_path'] 
+    student_age_file_path  = os.environ['student_age_file_path']
 except NameError:
     student_age_file_path  = '/data/student_age.json'
 
 student_age_file = open(student_age_file_path, "r")
 student_age = json.load(student_age_file)
 
-@app.route('/pozos/api/v1.0/get_student_ages', methods=['GET'])
+@app.route('/b/5e9fa5952940c704e1dc697f', methods=['GET'])
 @auth.login_required
 def get_student_ages():
     return jsonify({'student_ages': student_age })
 
-@app.route('/pozos/api/v1.0/get_student_ages/<student_name>', methods=['GET'])
+@app.route('/b/5e9fa5952940c704e1dc697f/<student_name>', methods=['GET'])
 @auth.login_required
 def get_student_age(student_name):
     if student_name not in student_age :
@@ -50,10 +49,10 @@ def get_student_age(student_name):
       del student_age[student_name]
       with open(student_age_file_path, 'w') as student_age_file:
         json.dump(student_age, student_age_file, indent=4, ensure_ascii=False)
-    return age 
+    return age
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
